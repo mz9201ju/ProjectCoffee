c9755407659e162c0a52387a3a10d1e0ed3be32f
mz9201ju <omer.mnsu@yahoo.com>
2016-04-21 18:30:59 +0000

    admin fixed

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,7 +4,10 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessor :login
-
+    def self.authenticate_unsafely(user_name, password)
+      find(:first, :conditions => "user_name = '#{user_name}' AND password = '#{password}'")
+    end
+    
     def self.find_first_by_auth_conditions(warden_conditions)
       conditions = warden_conditions.dup
       if login = conditions.delete(:login)
@@ -13,6 +16,7 @@ class Admin < ActiveRecord::Base
         where(conditions).first
       end
     end
+
 
  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
 
